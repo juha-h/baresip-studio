9210e2b21afc3b384933ae124208d9f2b03f9f53
Juha Heinanen <jh@tutpro.com>
2018-05-08 14:29:20 +0300

    - added handling of call security including update of status icon - added security and zid variables to Call object - added media encryption variable to Account object

--- app/src/main/kotlin/com/tutpro/baresip/Call.kt
+++ app/src/main/kotlin/com/tutpro/baresip/Call.kt
@@ -5,4 +5,6 @@ import android.text.TextWatcher
 class Call(val ua: String, val call: String, val peerURI: String, var status: String,
            val dtmfWatcher: TextWatcher?) {
     var hold: Boolean = false
+    var security: Int = 0
+    var zid: String = ""
 }
