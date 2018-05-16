a5e89d6f6a0915eb57432d84e48acdcf44d3bb00
Juha Heinanen <jh@tutpro.com>
2018-05-16 18:16:12 +0300

    no need for aors array or aor_ua function

--- app/src/main/kotlin/com/tutpro/baresip/Account.kt
+++ app/src/main/kotlin/com/tutpro/baresip/Account.kt
@@ -1,10 +1,3 @@
 package com.tutpro.baresip
 
-class Account(val ua: String, val aoR: String, val mediaenc: String, var status: String) {
-    private var statusImage: Int = 0
-
-    init {
-        this.statusImage = 0
-    }
-
-}
+class Account(val ua: String, val aoR: String, val mediaenc: String, var status: String) {}
