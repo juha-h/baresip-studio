df68e6fc02fa0394c2375c3db5755d26b0b54044
Juha Heinanen <jh@tutpro.com>
2018-05-26 11:10:07 +0300

    click on call button when callee is empty set callee to latest call peer moved history activity companion object to history class\ moved history to main activity

--- app/src/main/kotlin/com/tutpro/baresip/History.kt
+++ app/src/main/kotlin/com/tutpro/baresip/History.kt
@@ -8,4 +8,32 @@ class History(val aor: String, val peerURI: String, val direction: String,
 
     val time: GregorianCalendar = GregorianCalendar()
 
+    companion object {
+
+        fun aorHistory(history: ArrayList<History>, aor: String): Int {
+            var size = 0;
+            for (h in history) {
+                if (h.aor == aor) size++
+            }
+            return size
+        }
+
+        fun aorRemoveHistory(history: ArrayList<History>, aor: String) {
+            for (h in history) {
+                if (h.aor == aor) {
+                    history.remove(h)
+                    return
+                }
+            }
+        }
+
+        fun aorLatestHistory(history: ArrayList<History>, aor: String): History? {
+            for (h in history.reversed())
+                if (h.aor == aor) return h
+            return null
+        }
+
+    }
+
+
 }
