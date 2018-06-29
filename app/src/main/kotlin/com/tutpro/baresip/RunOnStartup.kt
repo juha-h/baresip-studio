88d7635bf8815dd3a470c8fab667a491083604b9
Juha Heinanen <jh@tutpro.com>
2018-06-29 15:34:18 +0300

    run RunOnStartup also on Restart action

--- app/src/main/kotlin/com/tutpro/baresip/RunOnStartup.kt
+++ app/src/main/kotlin/com/tutpro/baresip/RunOnStartup.kt
@@ -4,11 +4,14 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 
 class RunOnStartup : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+        if ((intent.action == Intent.ACTION_BOOT_COMPLETED) or
+                (intent.action == "com.tutpro.baresip.Restart")) {
+            Log.d("Baresip", "Start baresip upon boot completed  or restart")
             val i = Intent(context, MainActivity::class.java)
             i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             val b = Bundle()
