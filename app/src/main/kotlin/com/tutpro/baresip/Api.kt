ae5578ede4ccd8cc698c5ff12724377d89fd5535
Juha Heinanen <jh@tutpro.com>
2018-08-27 09:32:10 +0300

    - execute native ua call functions in re thread in order to avoid crash when   call ends - cleaned some code - upgraded build gradle version

--- app/src/main/kotlin/com/tutpro/baresip/Api.kt
+++ app/src/main/kotlin/com/tutpro/baresip/Api.kt
@@ -5,6 +5,5 @@ object Api {
     external fun audio_codecs(): String
     external fun call_peeruri(callp: String): String
     external fun cmd_exec(cmd: String): Int
-    external fun uri_decode(uri: String): Boolean
 
 }
