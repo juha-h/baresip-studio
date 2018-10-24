6bc1cc0841d40ddb187c9f2ba74395727ce4e3b6
Juha Heinanen <jh@tutpro.com>
2018-10-24 14:13:51 +0300

    removed unused import

--- app/src/main/kotlin/com/tutpro/baresip/AccountsActivity.kt
+++ app/src/main/kotlin/com/tutpro/baresip/AccountsActivity.kt
@@ -3,7 +3,6 @@ package com.tutpro.baresip
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.os.SystemClock
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.MenuItem
