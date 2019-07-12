b1dd1d4214605c44b1253c4ee8aba22f08c6ba04
Juha Heinanen <jh@tutpro.com>
2019-07-12 15:12:47 +0300

    - Fixed auto-rejecting of new incoming call.

--- app/src/main/cpp/baresip.c
+++ app/src/main/cpp/baresip.c
@@ -1074,12 +1074,10 @@ Java_com_tutpro_baresip_Api_ua_1hangup(JNIEnv *env, jobject thiz,
     const uint16_t native_code = code;
     const char *native_reason = (*env)->GetStringUTFChars(env, reason, 0);
     LOGD("hanging up call %s/%s\n", native_ua, native_call);
-    re_thread_enter();
     if (strlen(native_reason) == 0)
         ua_hangup(ua, call, native_code, NULL);
     else
         ua_hangup(ua, call, native_code, native_reason);
-    re_thread_leave();
     (*env)->ReleaseStringUTFChars(env, javaUA, native_ua);
     (*env)->ReleaseStringUTFChars(env, javaCall, native_call);
     (*env)->ReleaseStringUTFChars(env, reason, native_reason);
