7b3f43638b5d7fa2db2178e1a345bda63845d21d
Juha Heinanen <jh@tutpro.com>
2020-04-14 17:05:28 +0300

    = Renamed RunOnStartup.kt class to BootCompletedReceive.kt.

--- app/src/main/kotlin/com/tutpro/baresip/BootCompletedReceiver.kt
+++ app/src/main/kotlin/com/tutpro/baresip/BootCompletedReceiver.kt
@@ -7,7 +7,7 @@ import android.os.Bundle
 
 import java.nio.charset.StandardCharsets
 
-class RunOnStartup : BroadcastReceiver() {
+class BootCompletedReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         Log.i("Baresip", "RunOnStartup received intent ${intent.action}")
