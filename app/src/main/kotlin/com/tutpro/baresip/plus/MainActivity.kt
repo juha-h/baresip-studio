0aa83c33356a572a6c1a4c5826b1bf94fa8994fb
Juha Heinanen <jh@tutpro.com>
2020-07-04 09:44:55 +0300

    Removed unused import.

--- app/src/main/kotlin/com/tutpro/baresip/plus/MainActivity.kt
+++ app/src/main/kotlin/com/tutpro/baresip/plus/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.tutpro.baresip.plus
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.KeyguardManager
 import android.app.NotificationManager
