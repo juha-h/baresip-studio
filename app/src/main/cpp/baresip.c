420a4a200b9ddae481f8deaf59cdfb4ec984b417
Juha Heinanen <jh@tutpro.com>
2020-11-20 17:46:30 +0200

    Added FALLBACK ua events

--- app/src/main/cpp/baresip.c
+++ app/src/main/cpp/baresip.c
@@ -139,10 +139,16 @@ static void ua_exit_handler(void *arg)
 static const char *ua_event_reg_str(enum ua_event ev)
 {
     switch (ev) {
-        case UA_EVENT_REGISTERING:      return "registering";
-        case UA_EVENT_REGISTER_OK:      return "registered";
-        case UA_EVENT_REGISTER_FAIL:    return "registering failed";
-        case UA_EVENT_UNREGISTERING:    return "unregistering";
+        case UA_EVENT_REGISTERING:
+            return "registering";
+        case UA_EVENT_REGISTER_OK:
+        case UA_EVENT_FALLBACK_OK:
+            return "registered";
+        case UA_EVENT_REGISTER_FAIL:
+        case UA_EVENT_FALLBACK_FAIL:
+            return "registering failed";
+        case UA_EVENT_UNREGISTERING:
+            return "unregistering";
         default: return "?";
     }
 }
@@ -221,9 +227,11 @@ static void ua_event_handler(struct ua *ua, enum ua_event ev,
         case UA_EVENT_REGISTERING:
         case UA_EVENT_UNREGISTERING:
         case UA_EVENT_REGISTER_OK:
+        case UA_EVENT_FALLBACK_OK:
             len = re_snprintf(event_buf, sizeof event_buf, "%s", ua_event_reg_str(ev));
             break;
         case UA_EVENT_REGISTER_FAIL:
+        case UA_EVENT_FALLBACK_FAIL:
             len = re_snprintf(event_buf, sizeof event_buf, "registering failed,%s", prm);
             break;
         case UA_EVENT_CALL_INCOMING:
@@ -475,9 +483,9 @@ static int runLoggingThread() {
     dup2(pfd[1], 1);
     dup2(pfd[1], 2);
 
-    if (pthread_create(&loggingThread, 0, loggingFunction, 0) == -1) {
-        return -1;
-    }
+    int ret = pthread_create(&loggingThread, 0, loggingFunction, 0);
+    if (ret != 0)
+        return ret;
 
     pthread_detach(loggingThread);
 
