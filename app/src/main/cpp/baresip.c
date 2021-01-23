0fd21864e9ff95777ed0a7ecc700b95ec5889a1a
Juha Heinanen <jh@tutpro.com>
2021-01-23 17:12:06 +0200

    Use account_aor instead of ua_aor

--- app/src/main/cpp/baresip.c
+++ app/src/main/cpp/baresip.c
@@ -588,7 +588,7 @@ Java_com_tutpro_baresip_plus_BaresipService_baresipStart(JNIEnv *env, jobject in
         ua = le->data;
         sprintf(ua_buf, "%lu", (unsigned long) ua);
         jstring javaUA = (*env)->NewStringUTF(env, ua_buf);
-        LOGD("adding UA for AoR %s/%s\n", ua_aor(ua), ua_buf);
+        LOGD("adding UA for AoR %s/%s\n", account_aor(ua_account(ua)), ua_buf);
         jmethodID uaAddId = (*env)->GetMethodID(env, g_ctx.mainActivityClz, "uaAdd",
                                                   "(Ljava/lang/String;)V");
         (*env)->CallVoidMethod(env, g_ctx.mainActivityObj, uaAddId, javaUA);
