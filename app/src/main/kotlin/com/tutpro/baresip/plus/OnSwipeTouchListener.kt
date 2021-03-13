f723b59fa4042ba7c766e77a71c8c0eabc0e75f7
Juha Heinanen <jh@tutpro.com>
2021-03-13 10:05:17 +0200

    Added scrolling of accounts via left/right swipes

---
+++ app/src/main/kotlin/com/tutpro/baresip/plus/OnSwipeTouchListener.kt
@@ -0,0 +1,71 @@
+package com.tutpro.baresip.plus
+
+import android.content.Context
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
+
+open class OnSwipeTouchListener(ctx: Context) : View.OnTouchListener {
+
+    private val gestureDetector: GestureDetector
+
+    companion object {
+        private val SWIPE_THRESHOLD = 100
+        private val SWIPE_VELOCITY_THRESHOLD = 100
+    }
+
+    init {
+        gestureDetector = GestureDetector(ctx, GestureListener())
+    }
+
+    override fun onTouch(view: View?, event: MotionEvent?): Boolean {
+        return gestureDetector.onTouchEvent(event)
+    }
+
+    private inner class GestureListener : GestureDetector.SimpleOnGestureListener() {
+
+        override fun onDown(event: MotionEvent): Boolean {
+            return true
+        }
+
+        override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velX: Float, velY: Float): Boolean {
+            var result = false
+            try {
+                val diffY = e2!!.y - e1!!.y
+                val diffX = e2.x - e1.x
+                if (Math.abs(diffX) > Math.abs(diffY)) {
+                    if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velX) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffX > 0) {
+                            onSwipeRight()
+                        } else {
+                            onSwipeLeft()
+                        }
+                        result = true
+                    }
+                } else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velY) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffY > 0) {
+                        onSwipeBottom()
+                        result = false
+                    } else {
+                        onSwipeTop()
+                        result = true
+                    }
+                }
+            } catch (e: Exception) {
+                Log.e("Baresip", "onFling exception $e")
+                e.printStackTrace()
+            }
+
+            return result
+        }
+
+    }
+
+    open fun onSwipeRight() {}
+
+    open fun onSwipeLeft() {}
+
+    open fun onSwipeTop() {}
+
+    open fun onSwipeBottom() {}
+}
