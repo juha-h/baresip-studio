1770af11daf37049db5a4a4fa72c60f13198cfe9
Juha Heinanen <jh@tutpro.com>
2021-05-09 19:52:35 +0300

    In account password inputs, allow showing of password toggle   also when orientation is landscape.

--- app/src/main/kotlin/com/tutpro/baresip/AccountActivity.kt
+++ app/src/main/kotlin/com/tutpro/baresip/AccountActivity.kt
@@ -2,6 +2,7 @@ package com.tutpro.baresip
 
 import android.app.Activity
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.AsyncTask
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
