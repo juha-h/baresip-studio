d21439ffd4b6f45be4726943d8b8668a37990828
Juha Heinanen <jh@tutpro.com>
2021-05-14 20:25:00 +0300

    Made some vals constants

--- app/src/main/kotlin/com/tutpro/baresip/BaresipService.kt
+++ app/src/main/kotlin/com/tutpro/baresip/BaresipService.kt
@@ -1164,25 +1164,25 @@ class BaresipService: Service() {
 
     companion object {
 
-        val STATUS_NOTIFICATION_ID = 101
-        val CALL_NOTIFICATION_ID = 102
-        val TRANSFER_NOTIFICATION_ID = 103
-        val MESSAGE_NOTIFICATION_ID = 104
+        const val STATUS_NOTIFICATION_ID = 101
+        const val CALL_NOTIFICATION_ID = 102
+        const val TRANSFER_NOTIFICATION_ID = 103
+        const val MESSAGE_NOTIFICATION_ID = 104
 
-        val STATUS_REQ_CODE = 1
-        val CALL_REQ_CODE = 2
-        val ANSWER_REQ_CODE = 3
-        val REJECT_REQ_CODE = 4
-        val TRANSFER_REQ_CODE = 5
-        val ACCEPT_REQ_CODE = 6
-        val DENY_REQ_CODE = 7
-        val MESSAGE_REQ_CODE = 8
-        val REPLY_REQ_CODE = 9
-        val SAVE_REQ_CODE = 10
-        val DELETE_REQ_CODE = 11
+        const val STATUS_REQ_CODE = 1
+        const val CALL_REQ_CODE = 2
+        const val ANSWER_REQ_CODE = 3
+        const val REJECT_REQ_CODE = 4
+        const val TRANSFER_REQ_CODE = 5
+        const val ACCEPT_REQ_CODE = 6
+        const val DENY_REQ_CODE = 7
+        const val MESSAGE_REQ_CODE = 8
+        const val REPLY_REQ_CODE = 9
+        const val SAVE_REQ_CODE = 10
+        const val DELETE_REQ_CODE = 11
 
-        val DEFAULT_CHANNEL_ID = "com.tutpro.baresip.default"
-        val HIGH_CHANNEL_ID = "com.tutpro.baresip.high"
+        const val DEFAULT_CHANNEL_ID = "com.tutpro.baresip.default"
+        const val HIGH_CHANNEL_ID = "com.tutpro.baresip.high"
 
         var isServiceRunning = false
         var isConfigInitialized = false
