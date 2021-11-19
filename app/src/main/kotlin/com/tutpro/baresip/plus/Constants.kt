8c078d0b7aa849fe9eb2bcef81a3638a0df623f8
Juha Heinanen <jh@tutpro.com>
2021-11-19 19:55:28 +0200

    Renamed permission constants

--- app/src/main/kotlin/com/tutpro/baresip/plus/Constants.kt
+++ app/src/main/kotlin/com/tutpro/baresip/plus/Constants.kt
@@ -5,9 +5,9 @@ const val TAG = "Baresip+"
 const val DEFAULT_CHANNEL_ID = "com.tutpro.baresip.plus.default"
 const val HIGH_CHANNEL_ID = "com.tutpro.baresip.plus.high"
 
-const val CALL_PERMISSION_REQUEST_CODE = 1
+const val MIC_PERMISSION_REQUEST_CODE = 1
 const val CONTACT_PERMISSION_REQUEST_CODE = 2
-const val VIDEO_CALL_PERMISSION_REQUEST_CODE = 3
+const val CAMERA_PERMISSION_REQUEST_CODE = 3
 
 const val STATUS_NOTIFICATION_ID = 101
 const val CALL_NOTIFICATION_ID = 102
