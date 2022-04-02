ba8945a98ced7be417d50edd879ed79f14f55f42
Juha Heinanen <jh@tutpro.com>
2022-04-02 08:39:23 +0300

    Due to Google's requirement, ask user's consent if Android contacts is chosen in Settings

--- app/src/main/kotlin/com/tutpro/baresip/plus/ConfigActivity.kt
+++ app/src/main/kotlin/com/tutpro/baresip/plus/ConfigActivity.kt
@@ -9,6 +9,7 @@ import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.os.PowerManager
+import android.text.method.LinkMovementMethod
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
@@ -337,7 +338,22 @@ class ConfigActivity : AppCompatActivity() {
             override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
                 contactsMode = contactsModeKeys[position]
                 if (contactsMode != "baresip" && !Utils.checkPermissions(applicationContext, contactsPermissions))
-                    requestPermissions(contactsPermissions, CONTACTS_PERMISSION_REQUEST_CODE)
+                    with(MaterialAlertDialogBuilder(this@ConfigActivity, R.style.AlertDialogTheme)) {
+                        setTitle(getText(R.string.consent_request))
+                        setMessage(getText(R.string.contacts_consent))
+                        setPositiveButton(getText(R.string.accept)) { dialog, _ ->
+                            dialog.dismiss()
+                            requestPermissions(contactsPermissions, CONTACTS_PERMISSION_REQUEST_CODE)
+                        }
+                        setNegativeButton(getText(R.string.deny)) { dialog, _ ->
+                            contactsSpinner.setSelection(contactsModeKeys.indexOf(oldContactsMode))
+                            dialog.dismiss()
+                        }
+                        show().apply {
+                            findViewById<TextView>(android.R.id.message)
+                                    ?.movementMethod = LinkMovementMethod.getInstance()
+                        }
+                    }
             }
             override fun onNothingSelected(parent: AdapterView<*>) {}
         }
