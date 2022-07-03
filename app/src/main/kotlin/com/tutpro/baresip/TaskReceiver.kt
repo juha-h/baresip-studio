23af06df74d08f22b5f860e814bf80534385a2b4
Juha Heinanen <jh@tutpro.com>
2022-07-03 18:33:41 +0300

    Task receiver REGISTER/UNREGISTER improvement

--- app/src/main/kotlin/com/tutpro/baresip/TaskReceiver.kt
+++ app/src/main/kotlin/com/tutpro/baresip/TaskReceiver.kt
@@ -24,21 +24,17 @@ class TaskReceiver : BroadcastReceiver() {
         when (intent.action) {
             "com.tutpro.baresip.REGISTER" -> {
                 Log.d(TAG, "TaskReceiver: registering $aor")
-                if (!Api.ua_isregistered(ua.uap)) {
-                    Api.account_set_regint(acc.accp, REGISTRATION_INTERVAL)
-                    Api.ua_register(ua.uap)
-                    acc.regint = Api.account_regint(acc.accp)
-                    AccountsActivity.saveAccounts()
-                }
+                Api.account_set_regint(acc.accp, REGISTRATION_INTERVAL)
+                Api.ua_register(ua.uap)
+                acc.regint = Api.account_regint(acc.accp)
+                AccountsActivity.saveAccounts()
             }
             "com.tutpro.baresip.UNREGISTER" -> {
                 Log.d(TAG, "TaskReceiver: un-registering $aor")
-                if (Api.ua_isregistered(ua.uap)) {
-                    Api.account_set_regint(acc.accp, 0)
-                    Api.ua_unregister(ua.uap)
-                    acc.regint = Api.account_regint(acc.accp)
-                    AccountsActivity.saveAccounts()
-                }
+                Api.account_set_regint(acc.accp, 0)
+                Api.ua_unregister(ua.uap)
+                acc.regint = Api.account_regint(acc.accp)
+                AccountsActivity.saveAccounts()
             }
             else -> return
         }
