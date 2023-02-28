b892dc20dc136399f9e128353000fa3dcbf89a00
Juha Heinanen <jh@tutpro.com>
2023-03-01 01:10:46 +0200

    Show proper contact also when asking about deleting or adding chat peer from/to contacts

--- app/src/main/kotlin/com/tutpro/baresip/ChatsActivity.kt
+++ app/src/main/kotlin/com/tutpro/baresip/ChatsActivity.kt
@@ -112,7 +112,7 @@ class ChatsActivity: AppCompatActivity() {
                 with (builder) {
                     setTitle(R.string.confirmation)
                     setMessage(String.format(getString(R.string.long_chat_question),
-                            Utils.friendlyUri(this@ChatsActivity, peer, account)))
+                            Utils.friendlyUri(this@ChatsActivity, peer, account, true)))
                     setNeutralButton(getText(R.string.cancel), dialogClickListener)
                     setNegativeButton(getText(R.string.delete), dialogClickListener)
                     setPositiveButton(getText(R.string.add_contact), dialogClickListener)
