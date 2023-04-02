966b4c2893743b0a53876002c5ba257d2da0a09d
Juha Heinanen <jh@tutpro.com>
2023-04-02 16:13:24 +0300

    New call hold and call transfer buttons

--- app/src/main/kotlin/com/tutpro/baresip/MainActivity.kt
+++ app/src/main/kotlin/com/tutpro/baresip/MainActivity.kt
@@ -424,7 +424,7 @@ class MainActivity : AppCompatActivity() {
                 Log.d(TAG, "AoR $aor resuming call ${call.callp} with ${callUri.text}")
                 call.resume()
                 call.onhold = false
-                holdButton.setImageResource(R.drawable.hold)
+                holdButton.setImageResource(R.drawable.call_hold)
             } else {
                 Log.d(TAG, "AoR $aor holding call ${call.callp} with ${callUri.text}")
                 call.hold()
@@ -2050,7 +2050,7 @@ class MainActivity : AppCompatActivity() {
                     if (call.onhold) {
                         holdButton.setImageResource(R.drawable.resume)
                     } else {
-                        holdButton.setImageResource(R.drawable.hold)
+                        holdButton.setImageResource(R.drawable.call_hold)
                     }
                     callUri.inputType = InputType.TYPE_CLASS_PHONE
                     dialpadButton.setImageResource(R.drawable.dialpad_on)
