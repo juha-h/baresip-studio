27975e08f63035de0d15699bc6a43b18468a4c29
Juha Heinanen <jh@tutpro.com>
2024-02-16 00:48:28 +0200

    By default, allow inbound MESSAGEs

--- app/src/main/kotlin/com/tutpro/baresip/Account.kt
+++ app/src/main/kotlin/com/tutpro/baresip/Account.kt
@@ -142,7 +142,7 @@ class Account(val accp: Long) {
         if (autoRedirect)
             res += ";sip_autoredirect=yes"
 
-        res += ";ptime=20;regint=${regint};regq=0.5;pubint=0;call_transfer=yes"
+        res += ";ptime=20;regint=${regint};regq=0.5;pubint=0;call_transfer=yes;inreq_allowed=yes"
 
         var extra = ""
 
