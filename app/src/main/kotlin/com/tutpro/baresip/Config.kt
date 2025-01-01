cda602b811605392e334078adabd4f209491d553
Juha Heinanen <jh@tutpro.com>
2025-01-01 07:47:33 +0200

    Do not load webrtc_aecm module is aec is provided by Android

--- app/src/main/kotlin/com/tutpro/baresip/Config.kt
+++ app/src/main/kotlin/com/tutpro/baresip/Config.kt
@@ -20,7 +20,6 @@ object Config {
         if (!File(configPath).exists()) {
             for (module in AudioActivity.audioModules)
                 config = "${config}module ${module}.so\n"
-            if (!BaresipService.aec)
                 config = "${config}module webrtc_aecm.so\n"
             previousConfig = config
         } else {
