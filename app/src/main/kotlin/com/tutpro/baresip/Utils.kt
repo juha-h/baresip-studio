8201d9d890d56e5484e55affda810c9cd25c4fc2
Juha Heinanen <jh@tutpro.com>
2025-02-13 23:09:59 +0200

    In friendlyUri, replace %23 with #

--- app/src/main/kotlin/com/tutpro/baresip/Utils.kt
+++ app/src/main/kotlin/com/tutpro/baresip/Utils.kt
@@ -136,6 +136,7 @@ object Utils {
             if (u != e164Uri)
                 return u
         }
+        u = u.replace("%23", "#")
         if (u.contains("@")) {
             val user = uriUserPart(u)
             val host = uriHostPart(u)
