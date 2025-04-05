4906d37f43fe9f99ab13dd272cf3ed7714e6ce18
Juha Heinanen <jh@tutpro.com>
2025-04-05 13:32:25 +0300

    Minify release but don't obfuscate

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,8 @@ android {
     }
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -9,6 +9,7 @@
 
 # Add any project specific keep options here:
 -keepattributes LineNumberTable,SourceFile
+-dontobfuscate
 
 # If your project uses WebView with JS, uncomment the following
 # and specify the fully qualified class name to the JavaScript interface
