183fc3f7c863cb85a4ce8c201e0d5500405503e6
Juha Heinanen <jh@tutpro.com>
2025-04-05 13:37:10 +0300

    Minify release but don't obfuscate

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,8 @@ android {
     }
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
