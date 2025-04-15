d56d0aa0c7073651af931fc4783da16eec25b104
Juha Heinanen <jh@tutpro.com>
2025-04-15 18:18:21 +0300

    Removed unused dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,7 +74,6 @@ composeCompiler {
 dependencies {
     implementation(libs.androidx.foundation.android)
     implementation(libs.androidx.runtime.livedata)
-    implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.material3)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
