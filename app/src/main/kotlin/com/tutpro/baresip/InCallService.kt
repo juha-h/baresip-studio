8f7ddf81ca286c91da50f3462129ddc9bd9a2e15
Juha Heinanen <jh@tutpro.com>
2025-04-24 16:12:55 +0300

    Avoid crash in IncallService bind

--- app/src/main/kotlin/com/tutpro/baresip/InCallService.kt
+++ app/src/main/kotlin/com/tutpro/baresip/InCallService.kt
@@ -1,14 +1,16 @@
 package com.tutpro.baresip
 
 import android.app.Service
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.IBinder
 
 // This is needed in order to allow choosing baresip as default Phone app
 
 class InCallService : Service() {
-
-    override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+    override fun onBind(intent: Intent): IBinder? {
+        // TODO("Return the communication channel to the service.")
+        Log.d(TAG, "InCallService onBind with intent: ${intent.action}")
+        return null
     }
 }
