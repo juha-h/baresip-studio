3d2c1b3fefc6b5a9a2521cd70b0bb034c74536ce
Juha Heinanen <jh@tutpro.com>
2025-04-27 15:20:19 +0300

    Added support for numeric keyboard account setting (UI is still missing)

--- app/src/main/kotlin/com/tutpro/baresip/MainActivity.kt
+++ app/src/main/kotlin/com/tutpro/baresip/MainActivity.kt
@@ -118,6 +118,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -1078,7 +1079,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(start = 4.dp, end = 4.dp, top = 12.dp, bottom = 2.dp)
-                        .focusRequester(focusRequester),
+                        .focusRequester(focusRequester)
+                        .onFocusChanged {
+                            val account = Account.ofAor(viewModel.selectedAor.value)
+                            if (account != null) {
+                                dialpad = account.numericKeyboard
+                            }
+                        },
                     label = {
                         LabelText(
                             text = callUriLabel.value,
