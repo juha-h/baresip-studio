8e7f28dbdf440907d7480b7218fbcf2f7bf7eec8
Juha Heinanen <jh@tutpro.com>
2025-05-01 09:40:23 +0300

    New version 66.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.tutpro.baresip"
         minSdk = 28
         targetSdk = 35
-        versionCode = 429
-        versionName = "66.1.1"
+        versionCode = 430
+        versionName = "66.1.2"
         externalNativeBuild {
             cmake {
                 cFlags += "-DHAVE_INTTYPES_H -lstdc++"
