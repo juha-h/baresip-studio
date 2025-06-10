f360e4b8c64b36d2c789d9c02b9df36b0198b494
Juha Heinanen <jh@tutpro.com>
2025-06-10 13:19:59 +0300

    Minor full description edit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.tutpro.baresip"
         minSdk = 28
         targetSdk = 35
-        versionCode = 437
-        versionName = "67.0.1"
+        versionCode = 438
+        versionName = "67.0.2"
         externalNativeBuild {
             cmake {
                 cFlags += "-DHAVE_INTTYPES_H -lstdc++"
