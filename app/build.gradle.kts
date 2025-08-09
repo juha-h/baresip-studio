e5011cb47eccfa2c90f67ad206f328b118479d4e
Juha Heinanen <jh@tutpro.com>
2025-08-09 19:56:55 +0300

    Fixed build.gradle.kts lint warning Renamed settings.gradle to settings.gradele.kts Dependencies upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        jvmToolchain(17)
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.7"
