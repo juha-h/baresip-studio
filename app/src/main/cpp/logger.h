3772823421dbedc97d6b283dd071b9ef333c723f
Juha Heinanen <jh@tutpro.com>
2025-09-04 16:39:47 +0300

    Fixed baresip lib logging macros (credits to Alfred)

--- app/src/main/cpp/logger.h
+++ app/src/main/cpp/logger.h
@@ -13,14 +13,14 @@
 
     #define LOGI(...)                     \
         if (log_level_get() < LEVEL_WARN) \
-        ((void)__android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__))
+        ((void)__android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__))
 
     #define LOGW(...)                      \
         if (log_level_get() < LEVEL_ERROR) \
-        ((void)__android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__))
+        ((void)__android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__))
 
     #define LOGE(...)                       \
         if (log_level_get() <= LEVEL_ERROR) \
-        ((void)__android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__))
+        ((void)__android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__))
 
 #endif //BARESIP_LOGGER_H
