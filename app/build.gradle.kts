2ce08e246773feda864891c4dca6f6a55c134e56
Juha Heinanen <jh@tutpro.com>
2025-09-25 09:15:23 +0300

    New language Ukrainian and dependencies upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.tutpro.baresip.plus"
         minSdk = 28
         targetSdk = 36
-        versionCode = 218
-        versionName = "56.1.0"
+        versionCode = 219
+        versionName = "57.0.0"
         externalNativeBuild {
             cmake {
                 cFlags += "-DHAVE_INTTYPES_H -lstdc++"
