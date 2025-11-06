7be18847a930436d37f2f14983d663c209ae4d65
Juha Heinanen <jh@tutpro.com>
2025-11-06 03:32:26 +0200

    Use material color in info button

--- app/src/main/kotlin/com/tutpro/baresip/plus/MainScreen.kt
+++ app/src/main/kotlin/com/tutpro/baresip/plus/MainScreen.kt
@@ -1849,7 +1849,7 @@ private fun CallRow(ctx: Context, viewModel: ViewModel) {
                 Icon(
                     imageVector = ImageVector.vectorResource(id = R.drawable.info),
                     modifier = Modifier.size(36.dp),
-                    tint = Color.Unspecified,
+                    tint = MaterialTheme.colorScheme.secondary,
                     contentDescription = null,
                 )
             }
