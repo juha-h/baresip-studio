9f5265b0e5c786305c17e6ebdaffe13dbab15200
Juha Heinanen <jh@tutpro.com>
2025-11-24 09:54:24 +0200

    New version 59.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.tutpro.baresip.plus"
         minSdk = 28
         targetSdk = 36
-        versionCode = 226
-        versionName = "58.1.0"
+        versionCode = 227
+        versionName = "59.0.0"
         externalNativeBuild {
             cmake {
                 cFlags += "-DHAVE_INTTYPES_H -lstdc++"
