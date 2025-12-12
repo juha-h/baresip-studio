b88cb2c3f8edcdaab5bcd3cfdc4ce397f9afe2ba
Juha Heinanen <jh@tutpro.com>
2025-12-12 10:46:45 +0200

    Added missing codes and reasons to ua_hangup calls

--- app/src/main/kotlin/com/tutpro/baresip/plus/MainScreen.kt
+++ app/src/main/kotlin/com/tutpro/baresip/plus/MainScreen.kt
@@ -1487,9 +1487,9 @@ private fun CallRow(ctx: Context, viewModel: ViewModel) {
                         val callp = call.callp
                         Log.d(
                             TAG,
-                            "AoR ${ua.account.aor} hanging up call $callp with ${callUri.value}"
+                            "AoR ${ua.account.aor} canceling call $callp with ${callUri.value}"
                         )
-                        Api.ua_hangup(ua.uap, callp, 0, "")
+                        Api.ua_hangup(ua.uap, callp, 487, "Request Terminated")
                     }
                 },
             ) {
@@ -1516,7 +1516,7 @@ private fun CallRow(ctx: Context, viewModel: ViewModel) {
                         val call = uaCalls.first()
                         val callp = call.callp
                         Log.d(TAG, "AoR ${ua.account.aor} hanging up call $callp with ${callUri.value}")
-                        Api.ua_hangup(ua.uap, callp, 0, "")
+                        Api.ua_hangup(ua.uap, callp, 487, "Request Terminated")
                     }
                 }
             ) {
@@ -2323,7 +2323,7 @@ fun VideoLayout(ctx: Context, viewModel: ViewModel, onCloseVideo: () -> Unit) {
                         val uaCalls = ua.calls()
                         if (uaCalls.isNotEmpty()) {
                             val call = uaCalls.last()
-                            Api.ua_hangup(ua.uap, call.callp, 0, "")
+                            Api.ua_hangup(ua.uap, call.callp, 487, "Request Terminated")
                         }
                     }
                 ) {
@@ -2909,7 +2909,7 @@ fun handleServiceEvent(ctx: Context, viewModel: ViewModel, event: String, params
             val callp = params[1] as Long
             val call = Call.ofCallp(callp)
             if (call in Call.calls())
-                Api.ua_hangup(uap, callp, 0, "")
+                Api.ua_hangup(uap, callp, 487, "Request Terminated")
             call(ctx, viewModel, ua, ev[1], false)
             showCall(ctx, viewModel, ua)
         }
