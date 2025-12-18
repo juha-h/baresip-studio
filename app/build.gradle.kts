5cc4851c685ff58dc8d2692b9eb7ed23fd8554ab
Juha Heinanen <jh@tutpro.com>
2025-12-18 10:55:06 +0200

    New version 73.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.tutpro.baresip"
         minSdk = 28
         targetSdk = 36
-        versionCode = 462
-        versionName = "73.0.2"
+        versionCode = 463
+        versionName = "73.0.3"
         @Suppress("UnstableApiUsage")
         externalNativeBuild {
             cmake {
