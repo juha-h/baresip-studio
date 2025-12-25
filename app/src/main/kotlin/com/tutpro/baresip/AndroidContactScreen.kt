b08d24512afe88a153cc3888db3cd7263ac91258
Juha Heinanen <jh@tutpro.com>
2025-12-25 06:05:28 +0200

    Removed unused import

--- app/src/main/kotlin/com/tutpro/baresip/AndroidContactScreen.kt
+++ app/src/main/kotlin/com/tutpro/baresip/AndroidContactScreen.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.net.Uri
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
