0b867fa8f677063424d7070181185b658fbe7bfe
Juha Heinanen <jh@tutpro.com>
2026-04-01 10:12:48 +0300

    Added comment to InCallService

--- app/src/main/kotlin/com/tutpro/baresip/InCallService.kt
+++ app/src/main/kotlin/com/tutpro/baresip/InCallService.kt
@@ -5,11 +5,20 @@ import android.content.Intent
 import android.os.IBinder
 
 // This is needed in order to allow choosing baresip as default Phone app
-
 class InCallService : Service() {
     override fun onBind(intent: Intent): IBinder? {
-        // TODO("Return the communication channel to the service.")
-        Log.d("Baresip", "InCallService onBind with intent: ${intent.action}")
+        Log.d(TAG, "InCallService onBind with intent: ${intent.action}")
         return null
     }
 }
+
+/*import android.telecom.InCallService
+import android.telecom.Call
+
+class InCallService : InCallService() {
+    override fun onCallAdded(call: Call) {
+        super.onCallAdded(call)
+        // This is triggered when the system wants YOU to show the call UI
+        Log.d("Baresip", "InCallService: Call added")
+    }
+}*/
