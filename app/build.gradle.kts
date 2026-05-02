94d8a114edd4c0aedb21eebfee75db5e1b7a0e57
Juha Heinanen <jh@tutpro.com>
2026-05-02 12:30:31 +0300

    New version 68.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ configure<ApplicationExtension> {
         applicationId = "com.tutpro.baresip.plus"
         minSdk = 28
         targetSdk = 36
-        versionCode = 262
-        versionName = "68.2.0"
+        versionCode = 263
+        versionName = "68.3.0"
         @Suppress("UnstableApiUsage")
         externalNativeBuild {
             cmake {
