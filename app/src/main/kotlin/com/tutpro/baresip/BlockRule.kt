d516c8ce1853da2c945bfaa2e9a5e88a3ad6ee84
Juha Heinanen <jh@tutpro.com>
2026-06-21 20:18:53 +0300

    Added Block class and BlockingScreen

---
+++ app/src/main/kotlin/com/tutpro/baresip/BlockRule.kt
@@ -0,0 +1,49 @@
+package com.tutpro.baresip
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import java.io.File
+
+@Serializable
+class BlockRule(
+    val pattern: String
+) {
+    fun matches(uri: String): Boolean {
+        if (uri.contains(pattern, ignoreCase = true)) return true
+        return try {
+            Regex(pattern, RegexOption.IGNORE_CASE).containsMatchIn(uri)
+        } catch (_: Exception) {
+            false
+        }
+    }
+
+    companion object {
+        fun exists(pattern: String): Boolean {
+            return BaresipService.blockRules.any { it.pattern == pattern }
+        }
+
+        fun save() {
+            Log.d(TAG, "Saving ${BaresipService.blockRules.size} block rules")
+            val file = File(BaresipService.filesPath + "/blocking.json")
+            try {
+                val jsonString = Json.encodeToString(BaresipService.blockRules)
+                file.writeText(jsonString)
+            } catch (e: Exception) {
+                Log.e(TAG, "Serialization exception", e)
+            }
+        }
+
+        fun restore() {
+            val file = File(BaresipService.filesPath + "/blocking.json")
+            if (file.exists())
+                try {
+                    val jsonString = file.readText()
+                    BaresipService.blockRules = Json.decodeFromString<MutableList<BlockRule>>(jsonString)
+                    Log.d(TAG, "Restored ${BaresipService.blockRules.size} block rules")
+                } catch (e: Exception) {
+                    Log.e(TAG, "Deserialization exception: $e")
+                }
+        }
+
+    }
+}
