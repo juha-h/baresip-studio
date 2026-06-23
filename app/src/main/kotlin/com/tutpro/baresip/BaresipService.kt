d8d73e14ae5a6956847ab6c13431315b6c209ba8
Juha Heinanen <jh@tutpro.com>
2026-06-23 09:54:51 +0300

    Remove incoming call notification when call is declined

--- app/src/main/kotlin/com/tutpro/baresip/BaresipService.kt
+++ app/src/main/kotlin/com/tutpro/baresip/BaresipService.kt
@@ -636,6 +636,7 @@ class BaresipService: Service() {
             "Call Reject" -> {
                 val callp = intent!!.getLongExtra("callp", 0L)
                 val call = Call.ofCallp(callp)
+                nm.cancel(CALL_NOTIFICATION_ID)
                 stopRinging()
                 stopMediaPlayer()
                 if (call == null) {
