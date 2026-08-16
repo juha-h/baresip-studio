3c19160bcb7be2af6c68feed4634fe902fa6fdd0
Juha Heinanen <jh@tutpro.com>
2026-08-16 20:21:49 +0300

    Fixed error that sometimes caused two mobile accounts to appear after restart

--- app/src/main/kotlin/com/tutpro/baresip/Utils.kt
+++ app/src/main/kotlin/com/tutpro/baresip/Utils.kt
@@ -154,6 +154,7 @@ object Utils {
     fun uriMatch(firstUri: String, secondUri: String): Boolean {
         val first = uriUnescape(firstUri.removePrefix("<").removeSuffix(">"))
         val second = uriUnescape(secondUri.removePrefix("<").removeSuffix(">"))
+        if (first.equals(second, ignoreCase = true)) return true
         val mobileAor = "sip:mobile@pstn"
         if (first.startsWith("tel:", ignoreCase = true)) {
             if (first.equals(second, ignoreCase = true)) return true
