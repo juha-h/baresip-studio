ca2bf2d43b129a669c6081e2f3dfeb902438379f
Juha Heinanen <jh@tutpro.com>
2026-08-31 13:45:05 +0300

    Use indexed lazy column keys in order to avoid crash when showing filtered suggestions

--- app/src/main/kotlin/com/tutpro/baresip/MainScreen.kt
+++ app/src/main/kotlin/com/tutpro/baresip/MainScreen.kt
@@ -52,7 +52,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
@@ -1317,10 +1317,10 @@ private fun CallUriRow(
                             horizontalAlignment = Alignment.Start,
                             state = lazyListState
                         ) {
-                            items(
+                            itemsIndexed(
                                 items = filteredSuggestions,
-                                key = { (contact, _, matchingUri) -> "${contact.id()}:${matchingUri?.uri ?: ""}" }
-                            ) { (contact, annotatedName, matchingUri) ->
+                                key = { index, item -> "${item.first.id()}:${item.third?.uri ?: ""}:${item.third?.label ?: ""}:$index" }
+                            ) { _, (contact, annotatedName, matchingUri) ->
                                 Box(
                                     modifier = Modifier
                                         .fillMaxWidth()
@@ -1745,10 +1745,10 @@ private fun CallRow(
                                                 horizontalAlignment = Alignment.Start,
                                                 state = lazyListState,
                                             ) {
-                                                items(
+                                                itemsIndexed(
                                                     items = filteredSuggestions,
-                                                    key = { (contact, _, matchingUri) -> "${contact.id()}:${matchingUri?.uri ?: ""}" }
-                                                ) { (contact, annotatedName, matchingUri) ->
+                                                    key = { index, item -> "${item.first.id()}:${item.third?.uri ?: ""}:${item.third?.label ?: ""}:$index" }
+                                                ) { _, (contact, annotatedName, matchingUri) ->
                                                     Box(
                                                         modifier = Modifier
                                                             .fillMaxWidth()
