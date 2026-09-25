10cbace67e3f8fb4118b84173d672e921d9a5227
Juha Heinanen <jh@tutpro.com>
2026-09-25 09:43:44 +0300

    During mobile call, show call timer at the end of call row

--- app/src/main/kotlin/com/tutpro/baresip/MainScreen.kt
+++ app/src/main/kotlin/com/tutpro/baresip/MainScreen.kt
@@ -1609,12 +1609,11 @@ private fun CallUriRow(
                 }
             }
         }
-        if (call != null && call.showCallTimer.value) {
+        if (call != null && !call.ua.account.isMobile && call.showCallTimer.value)
             CallTimer(
                 initialDurationSeconds = call.callDuration.toLong(),
                 modifier = Modifier.padding(start = 6.dp, top = 6.dp)
             )
-        }
     }
 }
 
@@ -2123,6 +2122,13 @@ private fun CallRow(
                         )
                     }
                 )
+
+            if (call.ua.account.isMobile && call.showCallTimer.value)
+                CallTimer(
+                    initialDurationSeconds = call.callDuration.toLong(),
+                    modifier = Modifier.padding(end = 2.dp, top = 6.dp)
+                )
+
             if (call.showHangupButton.value)
                 LaunchedEffect(shouldRequestFocus) {
                     if (shouldRequestFocus) {
